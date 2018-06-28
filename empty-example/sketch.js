11aab6b8597617cefcf14aec2c7c42570faac31c
Glenn Lanzer <lanzertrey@gmail.com>
2018-06-28 18:37:43 -0500

    Init: main files for p5.js

---
+++ empty-example/sketch.js
@@ -0,0 +1,7 @@
+function setup() {
+  // put setup code here
+}
+
+function draw() {
+  ellipse(50, 50, 80, 80) // draw an ellipse, with the center 50px over from the left and 50px down from the top, with a width and height of 80px.
+}
